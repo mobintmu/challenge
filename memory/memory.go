578d64436b7e3ab3399a0a237e19cd3a1d85ee14
Mobin Shaterian <mobinest@gmail.com>
2024-08-04 19:21:36 +0330

    [Add] Update LRU

--- memory/memory.go
+++ memory/memory.go
@@ -27,3 +27,26 @@ func (s *InMemoryStorage) hash(key string) uint32 {
 	h.Write([]byte(key))
 	return h.Sum32()
 }
+
+func (s *InMemoryStorage) updateLRU(currentData *data) {
+
+	for index, value := range s.lastUsed {
+		if value.key == currentData.key {
+			copy(s.lastUsed[index:], s.lastUsed[index+1:])
+			s.lastUsed = s.lastUsed[:len(s.lastUsed)-1]
+			break
+		}
+	}
+	s.lastUsed = append([]*data{currentData}, s.lastUsed...)
+}
+
+func (s *InMemoryStorage) printLastUsed() {
+	println(">>>>>>>>>>>>>>>>>>>")
+	for index, value := range s.lastUsed {
+		println(index, " : ", value.key)
+	}
+	println("<<<<<<<<<<<<<<<<<<<")
+
+}
+
+func (s *InMemoryStorage) 
--- memory/memory_test.go
+++ memory/memory_test.go
@@ -1,15 +1,50 @@
 package memory
 
 import (
-	"fmt"
 	"testing"
+	"time"
 )
 
 func TestCreateHash(t *testing.T) {
-	fmt.Println("hi")
 	memory := NewInMemoryStorage(5, 100)
 	hash := memory.hash("hi")
 	if hash != 1748694682 {
 		t.Errorf("Hash function is not working as expected")
 	}
+}
+
+func TestAddUpdateLRU(t *testing.T) {
+	A := &data{
+		key:       "hi",
+		value:     "1212",
+		timestamp: time.Now().Add(1 * time.Hour).Unix(),
+	}
+
+	B := &data{
+		key:       "bye",
+		value:     "1312",
+		timestamp: time.Now().Add(1 * time.Hour).Unix(),
+	}
+
+	C := &data{
+		key:       "hello",
+		value:     "1234",
+		timestamp: time.Now().Add(1 * time.Hour).Unix(),
+	}
+
+	myMemory := NewInMemoryStorage(5, 100)
+	myMemory.updateLRU(A)
+	myMemory.updateLRU(B)
+	myMemory.updateLRU(C)
+	myMemory.updateLRU(A)
+	myMemory.updateLRU(A)
+	myMemory.updateLRU(C)
+	myMemory.updateLRU(A)
+
+	myMemory.printLastUsed()
+
+	if myMemory.lastUsed[0].key != "hi" && myMemory.lastUsed[1].key != "hello" && myMemory.lastUsed[2].key != "bye" {
+		t.Errorf("LRU is not working as expected")
+	}
+
 }
